885226cdbc96578adc1465c874d4509d6fbec069
Kunal <kunal.kumar@tothenew.com>
2017-05-27 10:30:35 +0530

    Initial Commit of spring application setup

---
+++ src/main/java/com/rabbitQueuing/RabbitQueuingApplication.java
@@ -0,0 +1,12 @@
+package com.rabbitQueuing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitQueuingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitQueuingApplication.class, args);
+	}
+}
